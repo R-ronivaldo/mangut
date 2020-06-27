0c969e562b945ecadccab9cd85c275f9fa23c858
junhfer <juniorferreira59@outlook.com>
2020-06-27 13:27:11 -0300

    adicionando cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 
 app = express();
@@ -14,6 +15,8 @@ mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true });
 //chamar todos os models
 requireDir("./src/model");
 
+
+app.use(cors());
 //chamando as rotas
 app.use("/", require("./src/routers/router"));
 
