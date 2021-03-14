e25db69f8e2f7166ac114fee86cdb92ec66fc58d
R-ronivaldo <juniorferreira59@outlook.com>
2021-03-14 06:59:12 -0300

    downgrde

--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,29 +1,9 @@
 const express = require("express");
-const redis = require("redis");
-const rateLimit = require("koa-ratelimit");
 
 const authenticateRouter = express.Router();
 
 const ControllerAuth = require("../../controller/ControllerAuth");
 
-var emailBasedRatelimit = ratelimit({
-    db: redis.createClient(),
-    duration: 60000,
-    max: 5,
-    id: function (context) {
-      return context.body.email;
-    }
-});
-
-var ipBasedRatelimit = ratelimit({
-    db: redis.createClient(),
-    duration: 60000,
-    max: 5,
-    id: function (context) {
-      return context.ip;
-    }
-});
-
-authenticateRouter.post("/", emailBasedRatelimit, ipBasedRatelimit ,ControllerAuth.acess);
+authenticateRouter.post("/", ControllerAuth.acess);
 
 module.exports = authenticateRouter;
