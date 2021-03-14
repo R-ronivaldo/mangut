308d530990a5ad5ae8c1e7df9b1ac1020983ca8d
R-ronivaldo <48776467+R-ronivaldo@users.noreply.github.com>
2021-03-14 04:02:39 -0300

    testando var de ambiente no heroku
    test

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const authConfig = require("../config/Auth.json");
+const authConfig = process.env.SECRETT
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
