e0141bad70e6f32a971f6c59ad3b9ae9d5816a0d
R-ronivaldo <juniorferreira59@outlook.com>
2021-03-14 06:52:46 -0300

    tentativa de criar protecao de forca bruta

--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const redis = require("redis");
+const rateLimit = require("koa-ratelimit");
 
 const authenticateRouter = express.Router();
 
 const ControllerAuth = require("../../controller/ControllerAuth");
 
-authenticateRouter.post("/", ControllerAuth.acess);
+var emailBasedRatelimit = ratelimit({
+    db: redis.createClient(),
+    duration: 60000,
+    max: 5,
+    id: function (context) {
+      return context.body.email;
+    }
+});
+
+var ipBasedRatelimit = ratelimit({
+    db: redis.createClient(),
+    duration: 60000,
+    max: 5,
+    id: function (context) {
+      return context.ip;
+    }
+});
+
+authenticateRouter.post("/", emailBasedRatelimit, ipBasedRatelimit ,ControllerAuth.acess);
 
 module.exports = authenticateRouter;
