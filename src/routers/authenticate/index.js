a085313f084d3e7166ef5f0d9e10c7e602fd18af
R-ronivaldo <juniorferreira59@outlook.com>
2021-03-15 01:07:48 -0300

    try deploy express-rate-limit

--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+
+const limitReached = (req, res) => {
+    log.warn({ ip: req.ip }, "Rate limiter triggered");
+    renderError(req, res) // Your function to render an error page
+}
+
+const ipLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10, // limit each IP to 100 requests per windowMs 
+    onLimitReached: limitReached, // called once when max is reached
+    handler: limitReached, // called for each subsequent request once max is reached
+});
 
 const authenticateRouter = express.Router();
 
 const ControllerAuth = require("../../controller/ControllerAuth");
 
-authenticateRouter.post("/", ControllerAuth.acess);
+authenticateRouter.post("/", ipLimiter, ControllerAuth.acess);
 
 module.exports = authenticateRouter;
