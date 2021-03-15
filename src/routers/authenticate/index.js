ef0f3de3feb8b9580b46f560a5fb703d90f1b9bd
R-ronivaldo <juniorferreira59@outlook.com>
2021-03-15 01:31:22 -0300

    try again deploy express-rate-limit

--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 
 const authenticateRouter = express.Router();
 
+const limiter = rateLimit({
+    windowMs: 1 * 30 * 1000,
+    max: 10,
+    handler: function(req, res){
+        res.status(405).send("Called for each subsequent request once max is reached");
+    },
+    onLimitReached: function(req, res){
+        res.status(405).send("Called for each subsequent request once max is reached");
+    }
+});
+
 const ControllerAuth = require("../../controller/ControllerAuth");
 
-authenticateRouter.post("/", ControllerAuth.acess);
+authenticateRouter.post("/", limiter, ControllerAuth.acess);
 
 module.exports = authenticateRouter;
